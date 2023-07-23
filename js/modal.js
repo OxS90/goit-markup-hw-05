e73f720b4ec3c674e54794c9f97b6d980ce39a9e
OxS90 <oxana.slivinschi@gmail.com>
2023-07-23 21:14:41 +0300

    finished

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,18 @@
 		openModalBtn: document.querySelector("[data-modal-open]"),
 		closeModalBtn: document.querySelector("[data-modal-close]"),
 		modal: document.querySelector("[data-modal]"),
+		overlay: document.getElementById("overlay"),
 	};
 
-	refs.openModalBtn.addEventListener("click", toggleModal);
-	refs.closeModalBtn.addEventListener("click", toggleModal);
+	refs.openModalBtn.addEventListener("click", openModal);
+	refs.closeModalBtn.addEventListener("click", closeModal);
 
-	function toggleModal() {
-		refs.modal.classList.toggle("is-hidden");
+	function openModal() {
+		refs.modal.classList.add("active");
+		refs.overlay.classList.add("active");
+	}
+	function closeModal() {
+		refs.modal.classList.remove("active");
+		refs.overlay.classList.remove("active");
 	}
 })();
